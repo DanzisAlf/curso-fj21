9a80e05495407193d367d59f8e5322476ee3d98c
DanzisAlf <migalha_fei@hotmail.com>
2017-08-19 10:33:28 -0300

    Exercicios 10.6 e 10.2

--- fj21-agenda/src/br/com/caelum/mvc/logica/RemoveContatoLogic.java
+++ fj21-agenda/src/br/com/caelum/mvc/logica/RemoveContatoLogic.java
@@ -1,5 +1,7 @@
 package br.com.caelum.mvc.logica;
 
+import java.sql.Connection;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,13 +14,15 @@ public class RemoveContatoLogic implements Logica {
 	public String executa(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		long id = Long.parseLong(request.getParameter("id"));
 		String nome = request.getParameter("nome");
-		
+
 		Contato contato = new Contato();
 		contato.setId(id);
 		contato.setNome(nome);
-		
-		ContatoDao dao = new ContatoDao();
-		System.out.println("excluindo contato " + contato.getNome() +" "+contato.getId());
+
+		Connection connection = (Connection) request.getAttribute("conexao");
+
+		ContatoDao dao = new ContatoDao(connection);
+		System.out.println("excluindo contato " + contato.getNome() + " " + contato.getId());
 		dao.exclui(contato);
 		return "mvc?logica=ListaContatosLogic";
 	}
